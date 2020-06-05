1fc532c6ad3bb74af461ab1b2980ab426d900767
JontyWang <501211312@qq.com>
2020-06-05 09:29:16 +0800

    添加项目文件。

---
+++ ViewComponents/CounterViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace Redis.ViewComponents
+{
+    public class CounterViewComponent:ViewComponent
+    {
+        private readonly IDatabase _db;
+
+        public CounterViewComponent(IConnectionMultiplexer redis)
+        {
+            _db = redis.GetDatabase();
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+        }
+    }
+}
